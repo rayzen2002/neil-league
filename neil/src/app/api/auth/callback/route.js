f256d8f680c2911f7cc3969117a8a9c105526674
Emanuel <emanunelzsmj@gmail.com>
2023-06-05 04:45:15 -0300

    redoing everything

--- neil/src/app/api/auth/callback/route.js
+++ neil/src/app/api/auth/callback/route.js
@@ -3,9 +3,8 @@ import QueryString from 'qs'
 import jwtDecode from 'jwt-decode'
 import { NextResponse } from 'next/server'
 import { api } from '@/lib/api'
-import jwt from 'jsonwebtoken'
 
-export async function GET(request, response) {
+export async function POST(request) {
   console.log(`oi`)
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
@@ -18,8 +17,11 @@ export async function GET(request, response) {
     grant_type: 'authorization_code',
     code,
   })
-  // const redirectTo = request.cookies.get('redirectTo')?.value
 
+  // const requestHeaders = {
+  //   Authorization: `Basic ${base64Credentials}`,
+  //   'Content-Type': 'application/x-www-form-urlencoded',
+  // }
   const headers = {
     Authorization: `Basic OTVlNGZhZjItZGIwZC00N2ZhLTkwNDMtM2EwN2Y5NTQ3Njg5OnlCdkpIelJFc3JWeTJub3NSSmxYTG1taGs1NThEMnZkdkdqem9BNVc=`,
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -39,29 +41,9 @@ export async function GET(request, response) {
     const { id_token } = tokenResponse.data
     const playerData = jwtDecode(id_token)
 
-    const token = jwt.sign(
-      {
-        nickname: playerData.nickname,
-        avatarUrl: playerData.picture,
-        email: playerData.email,
-      },
-      'supersecret', // Replace with your secret key
-      {
-        subject: playerData.guid,
-        expiresIn: '30 days',
-      },
-    )
-
-    // const redirectURL = redirectTo ?? new URL('/', request.url)
-
-    // response.setHeader('Set-Cookie', `token=${token}; Path=/; max-age=2592000;`)
-
-    // return NextResponse.redirect(redirectURL, {
-    //   headers: response.getHeaders(),
-    // })
-    return token
+    return NextResponse.json(playerData)
   } catch (error) {
     console.log(error)
-    return NextResponse.error(error.message || 'An error occurred')
+    // return NextResponse.json(error)
   }
 }
