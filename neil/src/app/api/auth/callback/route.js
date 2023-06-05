cef409133dde5ac55414e342b3b75c87c55d3bce
Emanuel <emanunelzsmj@gmail.com>
2023-06-04 22:56:52 -0300

    Corrigin rota do Oauth

--- neil/src/app/api/auth/callback/route.js
+++ neil/src/app/api/auth/callback/route.js
@@ -1,15 +1,35 @@
 import { api } from '@/lib/api'
 import { NextResponse } from 'next/server'
+import QueryString from 'qs'
 
 export async function GET(request) {
   console.log(`oi`)
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
+  const clientId = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+  const credentials = `${clientId}:${clientSecret}`
+
+  const base64Credentials = Buffer.from(credentials, 'utf-8').toString('base64')
+  const requestBody = QueryString.stringify({
+    grant_type: 'authorization_code',
+    code,
+  })
+
+  const headers = {
+    Authorization: `Basic ${base64Credentials}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
   console.log(code)
   try {
-    const registerResponse = await api.post('/register', {
-      code,
-    })
+    const registerResponse = await api.post(
+      '/register',
+      requestBody,
+      { headers },
+      {
+        code,
+      },
+    )
 
     const { token } = registerResponse.data
 
