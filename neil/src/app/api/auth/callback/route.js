3936696533d75e33365b5184b9fca8bc68c0959c
Emanuel <emanunelzsmj@gmail.com>
2023-06-05 13:41:15 -0300

    Adicionando banco de dados, e criando Player ao logar

--- neil/src/app/api/auth/callback/route.js
+++ neil/src/app/api/auth/callback/route.js
@@ -5,6 +5,7 @@ import { NextResponse } from 'next/server'
 import { api } from '@/lib/api'
 import jwt from 'jsonwebtoken'
 import dayjs from 'dayjs'
+import { prisma } from '@/lib/prisma'
 
 export async function GET(request) {
   console.log(`oi`)
@@ -64,10 +65,25 @@ export async function GET(request) {
     const redirectUrl = 'https://neildota.vercel.app'
     const expirationDate = dayjs().add(1, 'month').toDate()
     const cookie = `token=${token}; expires=${expirationDate.toUTCString()}; path=/`
-
-    return NextResponse.redirect(redirectUrl, {
-      headers: { 'Set-Cookie': cookie },
+    let player = await prisma.player.findUnique({
+      where: {
+        id: guid,
+      },
     })
+    if (!player) {
+      player = await prisma.player.create({
+        data: {
+          id: guid,
+          nickname,
+          avatarUrl: picture,
+          email,
+          name: `${given_name} ${family_name}`,
+        },
+      })
+      return NextResponse.redirect(redirectUrl, {
+        headers: { 'Set-Cookie': cookie },
+      })
+    }
   } catch (error) {
     console.log(error)
     return NextResponse.json(error)
