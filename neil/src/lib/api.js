7df4f1982e66bbdf039be9a4ac4c2d3d1b4732b7
Emanuel <emanunelzsmj@gmail.com>
2023-06-05 02:37:27 -0300

    Updating cors

--- neil/src/lib/api.js
+++ neil/src/lib/api.js
@@ -1,5 +1,5 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// export const api = axios.create({
-//   baseURL: 'https://neil-server.vercel.app/api/callback',
-// })
+export const api = axios.create({
+  baseURL: 'https://neil-server.vercel.app',
+})
