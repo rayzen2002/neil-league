0693c126a5e03f6d7566d705e63ecb86022669aa
Emanuel <emanunelzsmj@gmail.com>
2023-06-05 03:36:50 -0300

    Mudando para jsonwebtoken

--- neil/src/app/api/auth/callback/route.js
+++ neil/src/app/api/auth/callback/route.js
@@ -1,10 +1,11 @@
 /* eslint-disable camelcase */
 import QueryString from 'qs'
 import jwtDecode from 'jwt-decode'
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 import { api } from '@/lib/api'
+import jwt from 'jsonwebtoken'
 
-export async function GET(request) {
+export async function GET(request, response) {
   console.log(`oi`)
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
@@ -17,12 +18,8 @@ export async function GET(request) {
     grant_type: 'authorization_code',
     code,
   })
-  const redirectTo = request.cookies.get('redirectTo')?.value
+  // const redirectTo = request.cookies.get('redirectTo')?.value
 
-  // const requestHeaders = {
-  //   Authorization: `Basic ${base64Credentials}`,
-  //   'Content-Type': 'application/x-www-form-urlencoded',
-  // }
   const headers = {
     Authorization: `Basic OTVlNGZhZjItZGIwZC00N2ZhLTkwNDMtM2EwN2Y5NTQ3Njg5OnlCdkpIelJFc3JWeTJub3NSSmxYTG1taGs1NThEMnZkdkdqem9BNVc=`,
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -38,28 +35,24 @@ export async function GET(request) {
     const tokenResponse = await api.post(tokenEndpoint, requestBody, {
       headers,
     })
-    const fastify = require('fastify')()
-    fastify.register(require('@fastify/jwt'), {
-      secret: 'supersecret',
-    })
+
     const { id_token } = tokenResponse.data
     const playerData = jwtDecode(id_token)
-    const redirectURL = redirectTo ?? new URL('/', request.url)
-    const token = playerData.jwt.sign(
+
+    // const redirectURL = redirectTo ?? new URL('/', request.url)
+    const token = jwt.sign(
       {
-        name: id_token.name,
-        avatarUrl: id_token.avatarUrl,
+        nickname: playerData.nickname,
+        avatarUrl: playerData.avatarUrl,
+        email: playerData.email,
+        name: playerData.name,
       },
       {
-        sub: id_token.id,
+        sub: playerData.id,
         expiresIn: '30 days',
       },
     )
-    return NextResponse.redirect(redirectURL, {
-      headers: {
-        'Set-Cookie': `token=${token}; Path=/; max-age=2592000;`,
-      },
-    })
+    return token
   } catch (error) {
     console.log(error)
     // return NextResponse.json(error)
