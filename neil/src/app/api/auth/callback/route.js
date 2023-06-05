317fc9e0733bc651259d9423cd4df135b4de3ee4
Emanuel <emanunelzsmj@gmail.com>
2023-06-04 22:48:26 -0300

    Tratamento de erros no callback

--- neil/src/app/api/auth/callback/route.js
+++ neil/src/app/api/auth/callback/route.js
@@ -5,18 +5,22 @@ export async function GET(request) {
   console.log(`oi`)
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
+  console.log(code)
+  try {
+    const registerResponse = await api.post('/register', {
+      code,
+    })
 
-  const registerResponse = await api.post('/register', {
-    code,
-  })
+    const { token } = registerResponse.data
 
-  const { token } = registerResponse.data
-
-  const redirectUrl = new URL('/', request.url)
-  const cookieExpiresInSeconds = 30 * 24 * 60 * 60
-  return NextResponse.redirect(redirectUrl, {
-    headers: {
-      'Set-Cookie': `token=${token}; Path=/;max-age=${cookieExpiresInSeconds}`,
-    },
-  })
+    const redirectUrl = new URL('/', request.url)
+    const cookieExpiresInSeconds = 30 * 24 * 60 * 60
+    return NextResponse.redirect(redirectUrl, {
+      headers: {
+        'Set-Cookie': `token=${token}; Path=/;max-age=${cookieExpiresInSeconds}`,
+      },
+    })
+  } catch (error) {
+    return error.data
+  }
 }
