ef8f842917f6e9ff816f92c9ff069d56040b0aa9
Emanuel <emanunelzsmj@gmail.com>
2023-06-08 19:27:46 -0300

    Changing leaderboard to show by season

--- neil/src/app/leaderboards/page.jsx
+++ neil/src/app/leaderboards/page.jsx
@@ -1,5 +1,67 @@
+'use client'
 import { LeaderboardComponent } from '@/components/LeaderboardComponent'
+import { Medal } from 'lucide-react'
+import { useState } from 'react'
 
-export default async function Leaderboards() {
-  return <LeaderboardComponent />
+export default function Leaderboards() {
+  const [selectedSeason, setSelectedSeason] = useState('')
+  const [showLeaderBoard, setShowLeaderBoard] = useState(false)
+  const handleSelectChange = (e) => {
+    setSelectedSeason(e.target.value)
+  }
+  const handleButtonClick = (e) => {
+    e.preventDefault()
+
+    console.log(selectedSeason)
+    setShowLeaderBoard(true)
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={handleButtonClick}
+        className="mt-16 flex flex-col items-center justify-center gap-4"
+      >
+        <div className="flex flex-col items-center gap-2">
+          <Medal className="h-14 w-14 text-orange-500" />
+          <label
+            htmlFor="season"
+            className="rounded-full border-4 border-blitz-100 bg-blitz-400 p-4 text-3xl"
+          >
+            Escolha a temporada
+          </label>
+        </div>
+        <div className="flex gap-4">
+          <select
+            name="season"
+            id="season"
+            onChange={handleSelectChange}
+            className="text-bold flex items-center justify-center rounded-xl bg-blitz-400 text-center text-xl"
+          >
+            <option value="general">All time</option>
+            <option value="1">Season 1</option>
+            <option value="2">Season 2</option>
+            <option value="3">Season 3</option>
+            <option value="4">Season 4</option>
+            <option value="5">Season 5</option>
+            <option value="6">Season 6</option>
+            <option value="7">Season 7</option>
+            <option value="8">Season 8</option>
+            <option value="9">Season 9</option>
+            <option value="10">Season 10</option>
+            <option value="11">Season 11</option>
+            <option value="12">Season 12</option>
+            <option value="13">Season 13</option>
+            {/* Add more options for different seasons */}
+          </select>
+          <button className="rounded-xl  border-blitz-400 bg-blitz-400 p-2 text-3xl hover:bg-gray-200">
+            Mostrar{' '}
+          </button>{' '}
+        </div>
+      </form>
+
+      {/* Assign the handleButtonClick function to the onClick event */}
+      {showLeaderBoard ? <LeaderboardComponent season={selectedSeason} /> : ''}
+    </div>
+  )
 }
--- neil/src/components/LeaderboardComponent.jsx
+++ neil/src/components/LeaderboardComponent.jsx
@@ -1,12 +1,24 @@
-import { User } from 'lucide-react'
+import { Flame, User } from 'lucide-react'
 
-export const LeaderboardComponent = async () => {
+export const LeaderboardComponent = async (props) => {
+  let geralEndPoint = null
+  let page = 1
+  const pageSize = 50
   let leaderboards = [] // Declare and initialize the games variable
+  const { season } = props
 
   try {
-    const res = await fetch(
-      `https://open.faceit.com/data/v4/leaderboards/hubs/${process.env.NEXT_PUBLIC_HUB_ID}/seasons/13`,
-      {
+    while (leaderboards.length < 500) {
+      if (season === 'general') {
+        geralEndPoint = `https://open.faceit.com/data/v4/leaderboards/hubs/${
+          process.env.NEXT_PUBLIC_HUB_ID
+        }/general?limit=${pageSize}&offset=${(page - 1) * pageSize}`
+      }
+      const endpoint = `https://open.faceit.com/data/v4/leaderboards/hubs/${
+        process.env.NEXT_PUBLIC_HUB_ID
+      }/seasons/${season}?limit=${pageSize}&offset=${(page - 1) * pageSize}`
+      console.log(endpoint)
+      const res = await fetch(geralEndPoint ?? endpoint, {
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
           Accept: 'application/json',
@@ -14,45 +26,88 @@ export const LeaderboardComponent = async () => {
         next: {
           revalidate: 30,
         },
-      },
-    )
-    leaderboards = await res.json()
+      })
+      try {
+        const response = await res.json()
+        // Process the response and update the state or perform any necessary actions
+        if (response.items.length === 0) {
+          break // No more leaderboards, exit the loop
+        }
+        leaderboards = [...leaderboards, ...response.items]
+        page++
+      } catch (error) {
+        console.error(`Error occurred: ${error}`)
+        // Handle the error, display an error message, or take any other necessary actions
+      }
+    }
   } catch (error) {
     console.error(`Erro causado: ${error}`)
+    return error
   }
+
   return (
     <ul>
       <h1 className="mt-10 text-center text-5xl font-bold text-orange-500">
-        Season 13
+        Season {season}
       </h1>
-      {leaderboards && leaderboards.items && leaderboards.items.length > 0
-        ? leaderboards.items.map((items) => {
+      {leaderboards && leaderboards.length > 0
+        ? leaderboards.map((items) => {
             let winrate = 100 * items.win_rate
             if (!Number.isInteger(winrate)) {
               winrate = winrate.toFixed(2)
             }
             return (
               <div key={items.player.user_id}>
-                <li className="m-auto  mt-4  flex w-2/4 flex-col items-center justify-center gap-y-2 rounded-xl border-4  bg-blitz-400 pb-6">
-                  <div className=" flex items-center gap-4 pt-4 ">
-                    <User
-                      alt="avatar"
-                      className="h-28 w-28 rounded-full border-4 border-orange-500 "
-                    />
-
-                    <p className="text-2xl">
-                      Nickname :{' '}
-                      <span className="font-bold text-orange-500">
-                        {' '}
-                        {items.player.nickname}{' '}
-                      </span>
+                <li className="m-auto  mt-4  flex w-2/4 flex-col items-start justify-center gap-y-2 rounded-xl border-4  bg-blitz-400 pb-6">
+                  <div className=" mx-2 flex w-full items-center justify-start gap-12">
+                    <p className="flex items-start justify-start text-start text-5xl ">
+                      {items.position} º
                     </p>
+                    <div className="flex items-center gap-1">
+                      {items.player.avatar === '' ? (
+                        <User
+                          alt="avatar"
+                          className="h-20 w-20 rounded-full border-4 border-orange-500 "
+                        />
+                      ) : (
+                        <img
+                          alt="avatar"
+                          width={30}
+                          height={30}
+                          src={items.player.avatar}
+                          className="h-20 w-20 rounded-full border-4 border-orange-500"
+                        />
+                      )}
+
+                      <p className="text-2xl">
+                        <span className="font-bold text-orange-500">
+                          {items.player.nickname}
+                        </span>
+                      </p>
+                    </div>
                   </div>
-                  <p>Posicao : {items.position} º </p>
-                  <p>Partidas Jogadas : {items.played}</p>
-                  <p className="text-green-700">Ganhou : {items.won}</p>
-                  <p style={{ color: 'red' }}>Perdeu : {items.lost}</p>
-                  <p>Winrate : {`${winrate} %`}</p>
+                  <div className=" mx-12 grid  w-full grid-cols-2 ">
+                    <div>
+                      <p>Partidas Jogadas : {items.played}</p>
+                      <p className="text-green-700">Ganhou : {items.won}</p>
+                      <p style={{ color: 'red' }}>Perdeu : {items.lost}</p>
+                      <p>Winrate : {`${winrate} %`}</p>
+                    </div>
+                    <div className="flex flex-col items-start justify-start">
+                      <p>Pontos: {items.points}</p>
+                      <div className="flex gap-1">
+                        Streak :{' '}
+                        {items.current_streak < 4 ? (
+                          <p>+ {items.current_streak}</p>
+                        ) : (
+                          <p className="flex gap-4">
+                            + {items.current_streak}
+                            <Flame className="text-red-200 " />
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
                 </li>
               </div>
             )
